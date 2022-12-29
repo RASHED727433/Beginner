02dfe43a15e514a019c4b7d6b80087b41c33b428
RASHED727433 <mohammed727433@yahoo.com>
2022-12-29 15:44:35 +0530

    morecode

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import {Mohammed, Thankyou, Wjsxh1} from './mycomponentspractice/GreetClasscomponent';
+import { Component } from 'react';
+import { Practice, Xyz, Pppp, Yesyes, Tyre, Propsprops } from './mycomponentspractice/RashedGreet';
+import {Wjsx, Haha, Wjsx2} from './mycomponentspractice/Arrowfuncrashed';
+import {India, China, Australia, Germany, Malasia, Russia, Srilanka, Nepal, Japan} from './mycomponentspractice/prc';
+import {Megg, Checkout, Baggage, Counter, Counter2, Joker, Persn, Demmo, Teacups} from './mycomponentspractice/cevid10messg';
+
+// function App() {
+//   return ( <div> <Mohammed/> </div> )
+// }
+
+class App extends Component{
+  render(){
+    return ( <div>
+      <Teacups/>
+      {/* <Mohammed>class component</Mohammed>
+      <Xyz>Normal functional component</Xyz>
+      <Haha>An Arrow Function</Haha>
+      <Thankyou>another class component</Thankyou>
+      <Practice>An arrow function</Practice>
+      <Wjsx>no jsx</Wjsx>
+      <Wjsx2>playing</Wjsx2>
+      <Wjsxh1>Bolder no jsx</Wjsxh1>
+      <Pppp flavour="strawberry"/>
+      <Yesyes/>
+      <Tyre name="RASHED"/>
+      <Tyre name="ABRAR"/>
+      <Tyre name="MOHAMMED"/>
+      <Propsprops name = "parasite"/>
+      <India/>
+      <China/>
+      <Australia/>
+      <Germany/>
+      <Malasia/>
+      <Russia/>
+      <Srilanka/>
+      <Nepal place = "asia"/>
+      <Japan place = "asian"/> */}
+      {/* <Aka name = "Rashed" Age = "26" nationality = "Indian"/> */}
+      <Megg/>
+      <Checkout/>
+      <Baggage/>
+      <br></br>
+      <Counter/>
+      <Counter2/>
+      <Joker/>
+      <Persn name="Rashed" age = "26"/>
+      <Demmo locality = "Hyderabad" native = "india"/>
+      
+    </div>)
+  }
 }
 
-export default App;
+export default App
